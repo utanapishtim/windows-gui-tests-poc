0e50dc67ed9013282422de096aec8962511d9ea1
utanapishtim <ncawley@protonmail.com>
2021-12-06 11:24:17 -0800

    passing

--- main.js
+++ main.js
@@ -3,31 +3,24 @@ const path = require('path')
 const { app, BrowserWindow } = require('electron')
 const { port } = require('./port.json')
 app.commandLine.appendSwitch('remote-debugging-port', port)
-app.whenReady().then(() => {
-  createWindow()
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.whenReady().then(() => {
+  win()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) win()
   })
 })
 
+app.on('window-all-closed', function () { if (process.platform !== 'darwin') app.quit() })
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-function createWindow () {
-  const mainWindow = new BrowserWindow({
+function win () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  win.loadFile('index.html')
+  win.webContents.openDevTools()
 }
--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 const { spawn } = require('child_process')
 const cdp = require('chrome-remote-interface')
 const test = require('brittle')
-const { readFile } = require('fs/promises')
-const { pipeline, Writable } = require('streamx')
 const deepkill = require('deepkill')
 const { port } = require('./port.json')
 
@@ -11,11 +9,10 @@ if (process.platform === 'win32') cmd = 'npm.cmd'
 
 const child = spawn(cmd, ['start'])
 
-test('can read dom', async ({ plan, is, teardown }) => {
-  plan(1)
-  teardown(async () => {
-    await deepkill(child.pid)
-  })
+test('can see page and read dom', async ({ plan, ok, is, teardown }) => {
+  plan(2)
+  teardown(async () => { await deepkill(child.pid) })
+
   const targets = await (async function wait (start) {
     if (Date.now() - start > 30000) throw new Error('timeout')
     const targets = await cdp.List({ port }).catch(() => [])
@@ -24,11 +21,15 @@ test('can read dom', async ({ plan, is, teardown }) => {
   })(Date.now())
 
   const target = targets.find(({ url }) => /index\.html/.test(url))
+
+  ok(target)
+
   const client = await cdp({ target })
   await client.Page.enable()
   await client.DOM.enable()
   const { root: { nodeId } } = await client.DOM.getDocument()
   const node = await client.DOM.querySelector({ nodeId, selector: '#hello-world' })
   const { node: { localName } } = await client.DOM.describeNode({ nodeId: node.nodeId })
+
   is(localName, 'h1')
 })
